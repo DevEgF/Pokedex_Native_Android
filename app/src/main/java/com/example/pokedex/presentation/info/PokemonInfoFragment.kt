31216473ad342f24a24c5b4b980405021397d05c
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-06-07 22:10:08 -0300

    developer message

--- app/src/main/java/com/example/pokedex/presentation/info/PokemonInfoFragment.kt
+++ app/src/main/java/com/example/pokedex/presentation/info/PokemonInfoFragment.kt
@@ -1,6 +1,5 @@
 package com.example.pokedex.presentation.info
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,19 +13,10 @@ class PokemonInfoFragment : Fragment() {
         fun newInstance() = PokemonInfoFragment()
     }
 
-    private lateinit var viewModel: PokemonInfoViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_pokemon_info, container, false)
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(PokemonInfoViewModel::class.java)
-        //Use the ViewModel
-    }
-
 }
