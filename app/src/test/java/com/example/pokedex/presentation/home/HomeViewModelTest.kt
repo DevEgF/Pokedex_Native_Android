3a36a2a1abb8d5d0e4f123ce5de9fcb47546b39b
Egito Filho <104474051+DevEgF@users.noreply.github.com>
2023-07-10 21:00:30 -0300

    init unit test

--- app/src/test/java/com/example/pokedex/presentation/home/HomeViewModelTest.kt
+++ app/src/test/java/com/example/pokedex/presentation/home/HomeViewModelTest.kt
@@ -1,26 +1,72 @@
 package com.example.pokedex.presentation.home
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.paging.PagingData
+import androidx.paging.map
 import com.example.pokedex.data.usecase.GetPokemonUseCase
+import com.example.pokedex.utils.MainCoroutineRule
+import com.example.pokedex.utils.factory.PokemonFactory
+import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 import org.mockito.Mockito.*
 import org.mockito.junit.MockitoJUnitRunner
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @RunWith(MockitoJUnitRunner::class)
 class HomeViewModelTest {
 
-    @OptIn(ExperimentalCoroutinesApi::class)
-    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
-    private lateinit var pokemonViewModel: HomeViewModel
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
 
     @Mock
     lateinit var getPokemonUseCase: GetPokemonUseCase
+
+    private lateinit var pokemonViewModel: HomeViewModel
+
+    private val pokemonFactory = PokemonFactory()
+
+    private val pagingDataPokemon = PagingData.from(
+        listOf(
+            pokemonFactory.create(PokemonFactory.PokemonTest.Pokemon1),
+            pokemonFactory.create(PokemonFactory.PokemonTest.Pokemon2)
+        )
+    )
+
     @Before
     fun setup() {
-         pokemonViewModel = HomeViewModel(getPokemonUseCase)
+        pokemonViewModel = HomeViewModel(getPokemonUseCase)
     }
+
+    @Test
+    fun `should validate the paging data object values when calling pokemonPagingData`() =
+        runTest {
+            whenever(getPokemonUseCase.invoke(any()))
+                .thenReturn(
+                    flowOf(
+                        pagingDataPokemon
+                    )
+                )
+
+            val result = pokemonViewModel.pokemonPagingData("").first()
+
+            val expectedPagingData = pagingDataPokemon
+
+            result.map { pokemonResult ->
+                expectedPagingData.map {
+                    assertEquals(pokemonResult.name, pokemonResult.url)
+                }
+            }
+        }
 }
